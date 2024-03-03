c465fbcc032f7a37b1d8907361934ff652dced82
creamcookies <closingtime0502@gmail.com>
2024-03-03 16:04:13 +0900

    Adding mutex setting based on the WAV file.

--- hidden_wav_player.c
+++ hidden_wav_player.c
@@ -1,6 +1,7 @@
-#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
 
 void displayError(const char *message) {
     char errorMessage[256];
--- playwav.c
+++ playwav.c
@@ -1,45 +1,54 @@
-#include <windows.h>
 #include <stdbool.h>
-#define MUTEX_NAME "HiddenWavPlayerMutex"
+#include <string.h>
+#include <windows.h>
 
 
-bool isAnotherInstanceRunning() {
-  HANDLE hMutex = CreateMutex(NULL, FALSE, MUTEX_NAME);
-  if (hMutex == NULL) {
-    return true;
-  }
+bool isAnotherInstanceRunning(const char *filename) {
+    const char *lastBackslash = strrchr(filename, '\\');
+    const char *fileName =
+        (lastBackslash != NULL) ? lastBackslash + 1 : filename;
 
-  DWORD dwLastError = GetLastError();
-  if (dwLastError == ERROR_ALREADY_EXISTS) {
-    CloseHandle(hMutex);
-    return true;
-  }
+    char mutexName[MAX_PATH + sizeof("Mutex")];
+    strcpy(mutexName, fileName);
+    strcat(mutexName, "Mutex");
 
-  return false;
+    HANDLE hMutex = CreateMutex(NULL, FALSE, mutexName);
+    if (hMutex == NULL) {
+        return true;
+    }
+    DWORD dwLastError = GetLastError();
+    if (dwLastError == ERROR_ALREADY_EXISTS) {
+        CloseHandle(hMutex);
+        return true;
+    }
+
+    return false;
 }
 
 void playWav(const char *filename) {
-  PlaySound(filename, NULL, SND_FILENAME | SND_SYNC);
+    PlaySound(filename, NULL, SND_FILENAME | SND_SYNC);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-  if (isAnotherInstanceRunning()) {
-    return 1;
-  }
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+    if (__argc < 2 || __argc > 3) {
+        MessageBox(NULL, "Usage: <wav_file> [wait_time_in_seconds]", "Error",
+                   MB_OK | MB_ICONERROR);
+        return -1;
+    }
 
-  if (__argc < 2 || __argc > 3) {
-    MessageBox(NULL, "Usage: <wav_file> [wait_time_in_seconds]", "Error", MB_OK | MB_ICONERROR);
-    return -1;
-  }
+    const char *wav_file = __argv[1];
 
-  const char *wav_file = __argv[1];
+    if (isAnotherInstanceRunning(wav_file)) {
+        return 1;
+    }
 
-  if (__argc == 3) {
-    int wait_time = atoi(__argv[2]);
-    Sleep(wait_time * 1000);
-  }
+    if (__argc == 3) {
+        int wait_time = atoi(__argv[2]);
+        Sleep(wait_time * 1000);
+    }
 
-  playWav(wav_file);
+    playWav(wav_file);
 
-  return 0;
+    return 0;
 }
